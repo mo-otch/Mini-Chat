377e2b430728d88edd5b065224c240c59f52b4d1
Ahmed-63 <ahmed63djani@gmail.com>
2023-03-24 15:56:13 +0100

    first

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cloud'Chat - Login</title>
+    <title>Cloud'Chat Login</title>
 </head>
 <body>
     <div class="titre">
